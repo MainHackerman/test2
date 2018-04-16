fd0536b72b07c2603d3925697e7dd39a7afd756f
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-04-16 10:09:36 +0200

    nakupni seznam improved - work on project in progress

--- project.py
+++ project.py
@@ -1,39 +1,26 @@
-# rozklad na prvocinitele
-cislo = int(input('Zadej cislo: '))
-zbytek = cislo
-delitel = 2
+jmeno = input()
+budget = int(input())
 
-# prvocisla
-dva = 0
-tri = 0
-pet = 0
-sedm = 0
-jedenact = 0
-trinact = 0
-sedmnact = 0
-dvacettri = 0
+banan = 2
+jablko = 1
+hruska = 1
 
-# algoritmus
-while zbytek > 1:
-    while cislo % delitel != 0:
-        delitel = delitel + 1
-    if delitel == 2:
-        dva = dva + 1
-    if delitel == 3:
-        tri = tri + 1
-    if delitel == 5:
-        pet = pet + 1
-    if delitel == 7:
-        sedm = sedm + 1
-    if delitel == 11:
-        jedenact = jedenact + 1
-    if delitel == 13:
-        trinact = trinact + 1
-    if delitel == 17:
-        sedmnact = sedmnact + 1
-    if delitel == 23:
-        dvacettri = dvacettri + 1
-    print(delitel)
-    zbytek = zbytek/delitel
+cena = banan + jablko + hruska
 
-print(dva, tri, pet, sedm, jedenact, trinact, sedmnact, dvacettri)
+while cena > budget:
+    print('Prekrocil jsi budget, jak pokracovat?')
+    print('Zadej KONEC pro skonceni, BUDGET pro zmenu budgetu, NAKUP pro smazani polozky z nakupniho listu')
+    vyber = input()
+    if vyber == 'KONEC':
+        quit()
+    elif vyber == 'BUDGET':
+        budget = int(input('Zadej novou castku'))
+    elif vyber == 'NAKUP':
+        print('Jakou položku chceš odebrat?')
+        vyber = input()
+        if vyber == 'banan':
+            cena = cena - banan
+        elif vyber == 'jablko':
+            cena = cena - jablko
+        elif vyber == 'hruska':
+            cena = cena - hruska

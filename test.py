c880002e9aa5ce73a8cde16b8aa3a4c7323464f8
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-04-16 16:50:00 +0200

    test code optimalization

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 
 
 def CreateDataSet(list):
+    os.system('touch test_values')
     os.system('rm test_values')
     os.system('touch test_values')
     try:
@@ -14,37 +15,40 @@ def CreateDataSet(list):
     for item in list:
         os.system("echo " + str(item) + " >> test_values")
 
+def GetResults():
+    os.system('python3 project.py < test_values > test')
+    file = open('test')
+    string = file.read()
+    file.close()
+    return string.split('\n')
 
 def Test1(test_val):
     CreateDataSet(test_val)
-    os.system('python3 project.py < test_values > test')
-
-    file = open('test')
-    string = file.read()
-    file.close()
-    list = string.split('\n')
-    if list[-2] == 'Nákup se ruší.':
+    if GetResults()[-2] == 'Nákup se ruší.':
         print('PASS')
     else:
         print('FAIL')
     os.system('rm test')
+    os.system('rm test_values')
 
 def Test2(test_val):
     CreateDataSet(test_val)
-    os.system('python3 project.py < test_values > test')
-    file = open('test')
-    string = file.read()
-    file.close()
-    list = string.split('\n')
-    if 'Pepa' in list[-2] and '4' in list[-2]:
+    list = GetResults()
+    if test_val[0] in list[-2] and '4' in list[-2]:
         print('PASS')
     else:
         print('FAIL')
     os.system('rm test')
+    os.system('rm test_values')
 
+def Test3(test_val):
+    CreateDataSet(test_val)
+    print(GetResults())
 
 test1_val = ['Pepa', 2, 'KONEC']
 test2_val = ['Pepa', 2, 'BUDGET', 5]
+test3_val = ['Pepa', 2, 'NAKUP', 'jablko', 'NAKUP', 'jablko', 'KONEC']
 
 Test1(test1_val)
 Test2(test2_val)
+Test3(test3_val)

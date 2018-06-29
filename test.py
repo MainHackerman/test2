4883a046f332dc1aa967144d1072622d71f0bf36
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-06-30 01:15:43 +0200

    Removed colision beteween variable names and list constructor

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 
 fruit = ['jablko', 'hruska', 'banan']
 
+
 def CreateDataSet(list):
     os.system('touch test_values')
     os.system('rm test_values')
@@ -29,18 +30,20 @@ def ClearEnv():
     os.system('rm test')
     os.system('rm test_values')
 
+
 def Test0(test_val):
     CreateDataSet(test_val)
-    list = GetResults()
-    if test_val[0] in list[-2]:
+    lst = GetResults()
+    if test_val[0] in lst[-2]:
         os.system('rlPass "Name in output"')
     else:
         os.system('rlFail "Name not in output"')
-    if '5' in list[-2]:
+    if '5' in lst[-2]:
         os.system('rlPass "Price ok"')
     else:
         os.system('rlFail "Wrong price calculation"')
     ClearEnv()
+
 
 def Test1(test_val):
     CreateDataSet(test_val)
@@ -50,62 +53,67 @@ def Test1(test_val):
         os.system('rlFail "Cant cancel shopping"')
     ClearEnv()
 
+
 def Test2(test_val):
     CreateDataSet(test_val)
-    list = GetResults()
-    if test_val[0] in list[-2]:
+    lst = GetResults()
+    if test_val[0] in lst[-2]:
         os.system('rlPass "Can change the budget"')
     else:
         os.system('rlFail "Cant change the budget"')
-    if '5' in list[-2]:
+    if '5' in lst[-2]:
         os.system('rlPass "Price ok"')
     else:
         os.system('rlFail "Wrong price calculation"')
     ClearEnv()
 
+
 def Test3(test_val):
     for item in fruit:
         test_val[-1] = item
         CreateDataSet(test_val)
-        list = GetResults()
+        lst = GetResults()
         if item == 'banan':
-            if test_val[0] in list[-2] and '2' in list[-2]:
+            if test_val[0] in lst[-2] and '2' in lst[-2]:
                 os.system('rlPass "Can remove' + str(item) + 'from shopping list"')
-                #print('PASS - Can remove', item, 'from shopping list.')
+                # print('PASS - Can remove', item, 'from shopping list.')
             else:
                 os.system('rlFail "Cant remove' + str(item) + 'from shopping list"')
-                #print('FAIL - Cant remove', item, 'from shopping list.')
+                # print('FAIL - Cant remove', item, 'from shopping list.')
         else:
-            if test_val[0] in list[-2] and '4' in list[-2]:
+            if test_val[0] in lst[-2] and '4' in lst[-2]:
                 os.system('rlPass "Can remove' + str(item) + 'from shopping list"')
-                #print('PASS - Can remove', item, 'from shopping list.')
+                # print('PASS - Can remove', item, 'from shopping list.')
             else:
                 os.system('rlFail "Cant remove' + str(item) + 'from shopping list"')
-                #print('FAIL - Cant remove', item, 'from shopping list.')
+                # print('FAIL - Cant remove', item, 'from shopping list.')
         ClearEnv()
+
 
 def Test4(test_val):
     for item in fruit:
         test_val[3] = item
         test_val[-2] = item
         CreateDataSet(test_val)
-        list = GetResults()
-        if 'Tato položka není v seznamu' not in list:
+        lst = GetResults()
+        if 'Tato položka není v seznamu' not in lst:
             os.system('rlFail "Can remove' + str(item) + 'from shopping list twice"')
-            #print('FAIL - Can remove item', item, 'from shopping list twice.')
+            # print('FAIL - Can remove item', item, 'from shopping list twice.')
         else:
             os.system('rlPass "Cant remove' + str(item) + 'from shopping list twice"')
-            #print('PASS - Cannot remove', item, 'from shopping list twice.')
+            # print('PASS - Cannot remove', item, 'from shopping list twice.')
         ClearEnv()
+
 
 def Test5(test_val):
     CreateDataSet(test_val)
-    list = GetResults()
-    if 'Nákup se ruší' in list[-2]:
+    lst = GetResults()
+    if 'Nákup se ruší' in lst[-2]:
         os.system('rlPass "Shopping ends when everything is removed form the list"')
     else:
         os.system('rlFail "Shopping does not end when everything is removed form the list"')
     ClearEnv()
+
 
 test0_val = ['Pepa', 5]
 test1_val = ['Pepa', 2, 'KONEC']
